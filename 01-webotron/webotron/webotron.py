05aa4e2a1daa906c45f97c551eb708770647584a
tiago-moreira-solvimm <tiago.moreira@solvimm.com>
2021-04-04 10:35:12 -0300

    Setup a CDN domain using Cloudfront and ACM.

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -18,18 +18,22 @@ import boto3
 
 from bucket import BucketManager
 from domain import DomainManager
+from cdn import DistributionManager
+from certificate import CertificateManager
 import util
 
 session = None
 bucket_manager = None
 domain_manager = None
+cert_manager = None
+dist_manager = None
 
 
 @click.group()
 @click.option('--profile', default=None, help='Use a given AWS Profile.')
 def cli(profile):
     """Webotron deploys websites to AWS."""
-    global session, bucket_manager, domain_manager
+    global session, bucket_manager, domain_manager, cert_manager, dist_manager
 
     session_cfg = {}
     if profile:
@@ -38,6 +42,8 @@ def cli(profile):
     session = boto3.Session(**session_cfg)
     bucket_manager = BucketManager(session)
     domain_manager = DomainManager(session)
+    cert_manager = CertificateManager(session)
+    dist_manager = DistributionManager(session)
 
 
 @cli.command('list-buckets')
@@ -87,6 +93,36 @@ def setup_domain(domain):
     endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
     domain_manager.create_s3_domain_record(zone, domain, endpoint)
     print("Domain configured: http://{}".format(domain))
+
+@cli.command('find-cert')
+@click.argument('domain')
+def find_cert(domain):
+    """Find a certificate for <DOMAIN>."""
+    print(cert_manager.find_matching_cert(domain))
+
+@cli.command('setup-cdn')
+@click.argument('domain')
+@click.argument('bucket')
+def setup_cdn(domain, bucket):
+    dist = dist_manager.find_matching_dist(domain)
+
+    if not dist:
+        cert = cert_manager.find_matching_cert(domain)
+        if not cert: # SSL is not optional at this time.
+            print("Error: No matching cert found.")
+            return
+
+        dist = dist_manager.create_dist(domain, cert)
+        print("Waiting for distribution deloyment...")
+        dist_manager.await_deploy(dist)
+
+    zone = domain_manager.find_hosted_zone(domain) \
+        or domain_manager.create_hosted_zone(domain)
+
+    domain_manager.create_cf_domain_record(zone, domain, dist['DomainName'])
+    print("Domain configured: https://{}".format(domain))
+
+    return
 
 
 if __name__ == '__main__':
